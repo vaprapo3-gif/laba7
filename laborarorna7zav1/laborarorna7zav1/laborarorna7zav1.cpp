99851ac4c3dbfb13ac1a237d296668bb2abf5e13
vaprapo3-gif <vaprapo3@gmail.com>
2026-04-22 15:04:25 +0300

    Add files via upload

---
+++ laborarorna7zav1/laborarorna7zav1/laborarorna7zav1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+    ifstream inputFile("number.txt");
+
+    if (!inputFile) {
+        cerr << "Не вдалося відкрити файл для читання." << endl;
+        return 1;
+    }
+
+    double number;
+    double sum = 0;
+    int count = 0;
+
+    while (inputFile >> number) {
+        if (number < 0) {
+            sum += number;
+            count++;
+        }
+    }
+
+    inputFile.close();
+
+    if (count == 0) {
+        cout << "В файлі немає від’ємних чисел." << endl;
+    }
+    else {
+        cout << "Середнє арифметичне від’ємних чисел у файлі: "
+            << sum / count << endl;
+    }
+
+    return 0;
+}
